2ac20347f4e7e8c456ece565988ed039aefac0d2
Andrés Arnedo <andresarnedo2002@gmail.com>
2022-09-27 01:27:44 -0500

    .

--- config/default.py
+++
@@ -1,7 +0,0 @@
-# Configuración del email
-MAIL_SERVER = 'smtp-mail.outlook.com'
-MAIL_PORT = 587
-MAIL_USERNAME = 'aarnedoe@uninorte.edu.co'
-MAIL_PASSWORD = 'Andres.19'
-DONT_REPLY_FROM_EMAIL = '(Andres, aarnedoe@uninorte.edu.co)'
-ADMINS = ('aarnedoe@uninorte.edu.co', )
