b52dba1012081608a39e37d8dcc7c08e887e390f
Andrés Arnedo <andresarnedo2002@gmail.com>
2022-10-01 12:33:58 -0500

    Controlador de base de datos terminado

--- controllers/dbController.py
+++ controllers/dbController.py
@@ -1,34 +1,100 @@
 import sqlite3
 
+
 def init_db():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     return connection
 
 
-def insert(data):
+def register(data):
     connection = init_db()
     cursor = connection.cursor()
     try:
-        cursor.execute('SQL;')
+        cursor.execute("INSERT INTO usuario(nombreUsuario,email,password,estado) VALUES (?, ?, ?, ?)",
+                       [data.username, data.email, data.password, "no activado"])
         connection.commit()
         connection.close()
         return True
-    except:
+    except Exception as ex:
         connection.rollback()
         connection.close()
+        print(ex)
         return False
-    
 
 
-#SELECT
-def select(data):
+def login(data):
     connection = init_db()
-    cursor = connection.cursor() 
-    cursor.execute('SQL;')
-    rows = cursor.fetchall()
-    connection.close()
-    return rows
+    cursor = connection.cursor()
+    try:
+        cursor.execute(
+            "SELECT password FROM usuario WHERE email = ?", [data.email])
+        pwd = cursor.fetchone
+        return pwd
+    except Exception as ex:
+        print(ex)
+        return False
 
 
+def inbox(data):
+    connection = init_db()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM mensaje WHERE to_User = ?", [data.email])
+    return cursor.fetchall
 
+
+def sendMail(data):
+    connection = init_db()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("INSERT INTO mensaje(to_User,from_user,Asunto,cuerpo,estado,fecha)" +
+                       " VALUES (?, ?, ?, ?, ?", [data["to_user"], data["from_user"],
+                                                  data["asunto"], data["cuerpo"], data["estado"], data["fecha"]])
+        connection.commit()
+        connection.close()
+        return True
+    except Exception as ex:
+        connection.rollback()
+        connection.close()
+        print(ex)
+        return False
+
+
+def changePassword(data):
+    connection = init_db()
+    cursor = connection.cursor()
+    try:
+        cursor.execute("UPDATE usuario SET password = ? WHERE email = ?", 
+            [data.password, data.email])
+        connection.commit()
+        connection.close()
+        return True
+    except Exception as ex:
+        connection.rollback()
+        connection.close()
+        print(ex)
+        return False
+
+
+# def insert(data):
+#     connection = init_db()
+#     cursor = connection.cursor()
+#     try:
+#         cursor.execute('SQL;')
+#         connection.commit()
+#         connection.close()
+#         return True
+#     except:
+#         connection.rollback()
+#         connection.close()
+#         return False
+
+
+# SELECT
+# def select(data):
+#     connection = init_db()
+#     cursor = connection.cursor()
+#     cursor.execute('SQL;')
+#     rows = cursor.fetchall()
+#     connection.close()
+#     return rows
--- views/login.py
+++ views/login.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, session
 from controllers.pwdController import validatePwd
 
 login = Blueprint('login',__name__, template_folder='templates')
@@ -12,6 +12,8 @@ def loginPost():
     email = request.form["email"]
     pwd = request.form["pwd"]
     if(validatePwd(email,pwd)):
+        session["user"] = email
+        session["logged"] = True
         return "Inicio de sesión exitoso!"
     else:
         return "Error de autenticación!"         
