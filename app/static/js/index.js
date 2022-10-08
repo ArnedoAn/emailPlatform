c4e6f1283ac798cf6f6964d659afde40f7679628
mateo vieda <mvgs_328@hotmail.com>
2022-10-07 22:09:02 -0500

    plantillas agregadas

---
+++ app/static/js/index.js
@@ -0,0 +1,15 @@
+function registro(){
+    location.href = "registro"
+}
+
+function contraseña(){
+    location.href = "contraseña"
+}
+
+function cambcontraseña(){
+    location.href = "cambiocontraseña"
+}
+
+function iniciosesion(){
+    location.href = "/" 
+}
